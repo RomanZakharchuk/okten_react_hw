66211038fb8ecfea79cba873ed5ba1779e907d0c
Roman <70903505+RomanZakharchuk@users.noreply.github.com>
2022-07-26 15:00:58 +0300

    make task Ts

---
+++ app.ts
@@ -0,0 +1,98 @@
+interface IStarship {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    };
+    links: {
+        article_link: null;
+        video_link: string;
+    };
+    rocket: {
+        rocket_name: string;
+        first_stage: {
+            cores: [
+                {
+                    flight: number;
+                    core: {
+                        reuse_count: number;
+                        status: string;
+                    };
+                }
+            ]
+        };
+        second_stage: {
+            payloads: [
+                {
+                    payload_type: string;
+                    payload_mass_kg: number;
+                    payload_mass_lbs: number;
+                }
+            ]
+        }
+    }
+}
+
+const starship: IStarship = {
+    mission_name: 'Starlink-15 (v1.0)',
+    launch_date_local: '2020-10-24T11:31:00-04:00',
+    launch_site: {
+        site_name_long: 'Cape Canaveral Air Force Station Space Launch Complex 40',
+    },
+    links: {
+        article_link: null,
+        video_link: 'https://youtu/J442-ti-Dhg'
+    },
+    rocket: {
+        rocket_name: 'Falcon 9',
+        first_stage: {
+            cores: [
+                {
+                    flight: 7,
+                    core: {
+                        reuse_count: 6,
+                        status: 'unknown'
+                    }
+                }
+            ]
+        },
+        second_stage: {
+            payloads: [
+                {
+                    payload_type: 'Satellite',
+                    payload_mass_kg: 15400,
+                    payload_mass_lbs: 33951.2
+                }
+            ]
+        }
+    }
+};
+
+//
+
+//
+
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
+
+console.log(sum(4, 6));
+
+//
+
+const showSum = (a: number, b: number): void => {
+    console.log(a + b);
+}
+
+showSum(3, 8);
+
+//
+
+const user: {name: string, age: number, gender: string} = {name:'Roman', age:29, gender:'Male'}
+
+const incAge = (someUser:number, inc: number) => {
+    someUser += inc
+    return someUser
+}
+
+console.log(incAge(user.age, 2));
